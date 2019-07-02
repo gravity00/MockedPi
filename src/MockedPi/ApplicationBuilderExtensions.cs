29a8b845ce15fc5e1c08ff75bf1862b2738df015
João Simões <nogravity00@outlook.com>
2019-07-02 14:36:33 +0100

    Initial extensions setup

---
+++ src/MockedPi/ApplicationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using MockedPi;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Extension methods for <see cref="IApplicationBuilder"/> instances.
+    /// </summary>
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Maps a mock contition
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder MapMock(this IApplicationBuilder builder)
+        {
+            ParamAssert.NotNull(builder, nameof(builder));
+
+            return builder;
+        }
+    }
+}
---
+++ src/MockedPi/ParamAssert.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MockedPi
+{
+    internal static class ParamAssert
+    {
+        public static void NotNull<T>(T value, string paramName) where T : class
+        {
+            if (value == null) throw new ArgumentNullException(paramName);
+        }
+    }
+}
