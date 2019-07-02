a8fb0e6f4ccc59203222903e50c137725dfd8da5
João Simões <nogravity00@outlook.com>
2019-07-02 17:14:11 +0100

    Builder for response handlers

--- src/MockedPi/ApplicationBuilderExtensions.cs
+++ src/MockedPi/ApplicationBuilderExtensions.cs
@@ -10,23 +10,29 @@ namespace Microsoft.AspNetCore.Builder
     public static class ApplicationBuilderExtensions
     {
         /// <summary>
-        /// Maps a mock contition
+        /// Maps a mock to a given filter and, if matched, executes the action.
         /// </summary>
         /// <param name="builder"></param>
         /// <param name="filterConfig"></param>
+        /// <param name="actionConfig"></param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
         public static IApplicationBuilder MapMock(this IApplicationBuilder builder, 
-            Action<MockRequestFilter> filterConfig)
+            Action<MockRequestFilter> filterConfig, Action<MockRequestAction> actionConfig)
         {
             ParamAssert.NotNull(builder, nameof(builder));
             ParamAssert.NotNull(filterConfig, nameof(filterConfig));
+            ParamAssert.NotNull(actionConfig, nameof(actionConfig));
 
             var mockBuilder = new MockRequestFilter();
             filterConfig(mockBuilder);
             var predicate = mockBuilder.BuildPredicate();
 
-            return builder;
+            var actionBuilder = new MockRequestAction();
+            actionConfig(actionBuilder);
+            var handler = actionBuilder.BuildHandler();
+
+            return builder.MapWhen(ctx => predicate(ctx), b => b.Run(handler));
         }
     }
 }
---
+++ src/MockedPi/MockRequestAction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MockedPi
+{
+    /// <summary>
+    /// Request action when a filter is matched
+    /// </summary>
+    public class MockRequestAction
+    {
+        private readonly List<Func<HttpContext, Task>> _handlers = new List<Func<HttpContext, Task>>();
+
+        /// <summary>
+        /// Adds an handler to create a response
+        /// </summary>
+        /// <param name="handler"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public MockRequestAction Apply(Func<HttpContext, Task> handler)
+        {
+            ParamAssert.NotNull(handler, nameof(handler));
+
+            _handlers.Add(handler);
+
+            return this;
+        }
+
+        internal RequestDelegate BuildHandler()
+        {
+            if (_handlers.Count == 0)
+                throw new InvalidOperationException("Response handler collection is empty");
+
+            var handlers = _handlers.ToArray();
+            return async ctx =>
+            {
+                foreach (var handler in handlers)
+                    await handler(ctx);
+            };
+        }
+    }
+}
