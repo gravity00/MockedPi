4755f41688a53f320d5058b612f0ddbae1008888
João Simões <nogravity00@outlook.com>
2019-07-02 17:39:02 +0100

    Extension to assign status code

---
+++ src/MockedPi/MockRequestActionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MockedPi
+{
+    /// <summary>
+    /// Extensions for <see cref="MockRequestAction"/> instances.
+    /// </summary>
+    public static class MockRequestActionExtensions
+    {
+        /// <summary>
+        /// Sets the response status code
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="statusCode"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static MockRequestAction ApplyStatusCode(this MockRequestAction action, int statusCode)
+        {
+            ParamAssert.NotNull(action, nameof(action));
+
+            return action.Apply(ctx =>
+            {
+                ctx.Response.StatusCode = statusCode;
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
